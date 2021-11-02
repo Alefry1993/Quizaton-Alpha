1212315e9dcecdf6a87ad5e0b4cec0abf052b9b1
Aleksander <Alefry1993@gmail.com>
2021-11-02 20:09:11 +0100

    update

--- app/src/main/java/com/example/quizaton_alpha/Authentication/logInActivity.java
+++ app/src/main/java/com/example/quizaton_alpha/Authentication/logInActivity.java
@@ -1,7 +1,10 @@
 package com.example.quizaton_alpha.Authentication;
 
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class logInActivity extends AppCompatActivity {
+    private TextView 
 
 }
