ef01cb5d3d339a97a372c183faa4fc65cdd03df6
KirillGaydamak <92462807+KirillGaydamak@users.noreply.github.com>
2021-11-03 21:03:58 +0100

    Update thequiz.java

--- app/src/main/java/com/example/quizaton_alpha/Activities/thequiz.java
+++ app/src/main/java/com/example/quizaton_alpha/Activities/thequiz.java
@@ -6,8 +6,16 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.quizaton_alpha.R;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class thequiz extends AppCompatActivity {
+
+    FirebaseFirestore db;
+
+    static final String TAG = "Read Data Activity";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
