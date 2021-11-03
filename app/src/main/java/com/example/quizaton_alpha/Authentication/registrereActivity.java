039bd022f32b74b457de0742925a03b78d35609f
Aleksander <Alefry1993@gmail.com>
2021-11-03 19:19:10 +0100

    Firebase

--- app/src/main/java/com/example/quizaton_alpha/Authentication/registrereActivity.java
+++ app/src/main/java/com/example/quizaton_alpha/Authentication/registrereActivity.java
@@ -1,9 +1,36 @@
 package com.example.quizaton_alpha.Authentication;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quizaton_alpha.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class registrereActivity extends AppCompatActivity {
     Intent recievedIntent = getIntent();
+    private FirebaseAuth mAuth;
+    private ImageView logo;
+    private EditText regName, regPass, regEmail;
+    private Button regButton;
+    private ProgressBar regProg;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrer);
+        mAuth = FirebaseAuth.getInstance();
+
+        logo = (ImageView) findViewById(R.id.logo);
+        regEmail = (EditText) findViewById(R.id.email);
+        regEmail = (EditText) findViewById(R.id.email);
+        regEmail = (EditText) findViewById(R.id.email);
+    }
 }
