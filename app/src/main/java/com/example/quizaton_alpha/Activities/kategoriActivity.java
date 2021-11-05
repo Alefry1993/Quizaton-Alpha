a28da217fd6229a986626ce52d0ba794d691e422
KirillGaydamak <92462807+KirillGaydamak@users.noreply.github.com>
2021-11-05 12:22:58 +0100

    Geografi Quiz

--- app/src/main/java/com/example/quizaton_alpha/Activities/kategoriActivity.java
+++ app/src/main/java/com/example/quizaton_alpha/Activities/kategoriActivity.java
@@ -2,14 +2,21 @@ package com.example.quizaton_alpha.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.quizaton_alpha.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class kategoriActivity extends AppCompatActivity {
 
@@ -23,6 +30,7 @@ public class kategoriActivity extends AppCompatActivity {
     private Button blandet1Button;
     private Button blandet2Button;
 
+    private static final String TAG = "DocSnippets";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,11 +47,33 @@ public class kategoriActivity extends AppCompatActivity {
         blandet1Button = findViewById(R.id.blandet1Button);
         blandet2Button = findViewById(R.id.blandet2Button);
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
         geoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent startintent = new Intent(getApplicationContext(),thequiz.class);
                 startActivity(startintent);
+
+                DocumentReference docRef = db.collection("Kategori").document("Geografi");
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d(TAG, "DocumentSnapshot: " + document.getData());
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+
+
             }
         });
 
