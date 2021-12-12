1a0bf5e75424fc02a6f19d573481833606d05304
KirillGaydamak <92462807+KirillGaydamak@users.noreply.github.com>
2021-12-12 18:41:46 +0100

    startQuiz

--- app/src/main/java/com/example/quizaton_alpha/Activities/forsideActivity.java
+++ app/src/main/java/com/example/quizaton_alpha/Activities/forsideActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.quizaton_alpha.Authentication.logInActivity;
@@ -32,10 +33,13 @@ public class forsideActivity extends AppCompatActivity {
         profilButton = (Button) findViewById(R.id.profilButton);
 
 
+
+
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent startintent = new Intent(getApplicationContext(),StartActivity.class);
+                startintent.putExtra("QUIZ_KATEGORI", quizKategori);
                 startActivity(startintent);
             }
         });
@@ -63,6 +67,11 @@ public class forsideActivity extends AppCompatActivity {
                 startActivity(profilIntent);
             }
         });
-    }}
+    }
+
+    public void startQuiz(View view) {
+
+    }
+}
 
 
